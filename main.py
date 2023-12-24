01446177a3a65ce7eb7c87c904cd3ef517ab8e96
Caleb Buth <caleb.buth@gmail.com>
2023-12-24 11:59:38 -0700

    syncing

--- main.py
+++ main.py
@@ -23,25 +23,6 @@ async def on_ready():
  #   bot_latency = round(client.latency * 1000)
   #  await ctx.send(f"Pong! {bot_latency} ms.")
 
-@client.command()
-async def join(ctx):
-    if ctx.author.voice:
-        channel = ctx.author.voice.channel
-        try:
-            await channel.connect()
-        except Exception as e:
-            await ctx.send(f"An error occurred: {e}")
-    else:
-        await ctx.send("You aren't in a voice channel. Please join one to use this command.")
-
-@client.command()
-async def leave(ctx):
-    if ctx.voice_client:
-        await ctx.voice_client.disconnect()
-        await ctx.send("I left the voice channel.")
-    else:
-        await ctx.send("I'm not in a voice channel.")
-
 
 @client.command(help ="Shows a list of available commands.")
 async def helpme(ctx):
@@ -58,7 +39,7 @@ async def load():
 async def main():
     async with client:
         await load()
-        await client.start("MTE3NDkzMzUzMDA0MDU5ODYyOQ.GOFoEI.gSy8ZtaFiiVn-vhftFfCPUIuIAhv6SbbYL-gZM")
+        await client.start("")
 
 asyncio.run(main())
 
